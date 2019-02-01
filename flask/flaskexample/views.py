df2dbd53ba9ed1ca774ee3297ae1a19df74954bc
Stefan Heinze <stefanheinze@Stefans-MacBook-Pro.local>
2019-02-01 10:49:10 -0500

    updated website. implemented item features in recommendation system

--- flask/flaskexample/views.py
+++ flask/flaskexample/views.py
@@ -1,39 +1,87 @@
 from flask import render_template
-from flaskexample import app
 from flask import request
-from flaskexample.a_Model import ModelIt
-from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database
+from flaskexample import app
 import pandas as pd
 import numpy as np
-import psycopg2
-import recommendation
+import recommendation as rec
+import pickle
 from lightfm import data
-from lightfm import cross_validation
+import re
 
-columns = sorted(list(pd.read_pickle('column_names')))
+# Get list of dictionaries containing all hike names
+columns = sorted(list(pd.read_pickle('flaskexample/Data/column_names_190129')))
 hike_names = []
 for hike_name in columns:
     hike_names.append({'name': hike_name})
 
-data = pd.read_pickle('hike_data_filtered_190124')
-dataset, interactions = recommendation.lightfm_implicit_matrix(data)
-train, test = cross_validation.random_train_test_split(interactions, test_percentage=0.2, random_state=np.random.RandomState(seed=1))
-model = recommendation.lightfm_train(train, 30, 30)
+# Get list of dictionaries containing all feature names
+feature_matrix = pd.read_pickle('flaskexample/Data/hike_features_190129b')
+drop_columns = ['difficulty_easy', 'difficulty_hard', 'difficulty_moderate',
+               'distance_short', 'distance_medium', 'distance_long',
+               'elevation_easy', 'elevation_medium', 'elevation_hard',
+               'out_and_back', 'point_to_point', 'loop']
+tag_names = []
+for tag in feature_matrix.drop(labels=drop_columns, axis=1).columns:
+    tag_names.append({'name': tag.capitalize()})
 
+# Read in dataset and model 
+
+with open('flaskexample/Data/lightfm_dataset.pickle', 'rb') as f:
+    dataset = pickle.load(f)
+with open('flaskexample/Data/lightfm_model.pickle', 'rb') as f:
+    model = pickle.load(f)
+with open('flaskexample/Data/lightfm_featurematrix.pickle', 'rb') as f:
+    lfm_feature_matrix = pickle.load(f)
+
+item_features_map = dataset.mapping()[3] # get mapping for item features to lightfm indices
+item_features_embeddings = model.item_embeddings # get item feature embeddings
 item_map = dataset.mapping()[2]
+item_embeddings = model.get_item_representations(features=lfm_feature_matrix)[1]
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/input', methods=['POST', 'GET'])
 def index():
     return render_template("input.html",
-    hike_names = hike_names)
+    hike_names = hike_names,
+    tag_names = tag_names)
 
 @app.route('/output', methods=['POST', 'GET'])
 def output():
-   selected_hikes = request.form['hikes']
-   hike_index = item_map[selected_hikes]
-   sim_scores = recommendation.calculate_similarities(model, hike_index)
-   top_hikes = recommendation.get_top_hikes_by_similarity(sim_scores, item_map)
-   return render_template("output.html", result=top_hikes)
+    selected_hikes_text = request.form['hikes']
+    selected_hikes_list = selected_hikes_text.split('||')[:-1]
+    selected_hikes = []
+    for hike in selected_hikes_list:
+         s1 = re.sub('^,', '', hike)
+         s2 = re.sub('$,', '', s1)
+         selected_hikes.append(s2)
 
+    tags = [ tag.lower() for tag in request.form['tags'].split(',') ]
+    hike_difficulty = request.form['hikedifficulty']
+    hike_distance = request.form['hikedistance']
+    hike_elevation = request.form['hikeelevation']
+    hike_type = request.form['hiketype']
+
+    hike_attributes = rec.get_attributes(tags=tags, hike_difficulty=hike_difficulty, hike_distance=hike_distance,
+                      hike_elevation=hike_elevation, hike_type=hike_type)
+    selected_item_features_embeddings= rec.get_item_feature_embeddings(hike_attributes, item_features_map, 
+                                       item_features_embeddings)
+    selected_hike_embeddings = rec.get_item_embeddings(selected_hikes=['Inspiration Point'], item_map=item_map, 
+                               item_embeddings=item_embeddings)
+    av_hike_embedding = rec.get_average_item_embedding(selected_item_embeddings=selected_hike_embeddings)
+    final_embedding = rec.get_final_embedding(av_item_embedding=av_hike_embedding, 
+                      selected_item_features_embeddings=selected_item_features_embeddings)
+    sim_scores = rec.calculate_similarities(model, final_embedding, item_embeddings)
+    top_hikes = rec.get_top_hikes_by_similarity(sim_scores, item_map, num_hikes=10)
+   
+    matched_tags_for_hikes = {}
+    for hike in top_hikes:
+        hike_features = rec.get_hike_features(hike, feature_matrix)
+        convereted_hike_features = rec.convert_features(hike_features)
+        matched_tags = rec.get_matched_features(convereted_hike_features, hike_difficulty=hike_difficulty, 
+                                                hike_distance=hike_distance, hike_elevation=hike_elevation, 
+                                                hike_type=hike_type, tags=[ tag.capitalize() for tag in tags])
+        matched_tags_for_hikes[hike] = matched_tags
+     
+    return render_template("output.html", top_hikes=top_hikes, hike_difficulty = hike_difficulty,
+           hike_distance = hike_distance, hike_elevation = hike_elevation, hike_type = hike_type,
+           tags = tags, matched_tags_for_hikes = matched_tags_for_hikes)
--- flask/recommendation.py
+++ flask/recommendation.py
@@ -1,78 +1,185 @@
-import pandas as pd
 import numpy as np
-from lightfm import LightFM
-from scipy.sparse import coo_matrix
-from scipy import sparse
-from lightfm import data
-from lightfm import cross_validation
-from lightfm.cross_validation import random_train_test_split
-from lightfm.evaluation import auc_score
-from lightfm.evaluation import precision_at_k
 from numpy import dot
 from numpy.linalg import norm
 
-def lightfm_implicit_matrix(df):
-    dataset = data.Dataset() # Instantiate class
-    dataset.fit((user for user in df.index),
-                (feature for feature in df.columns)) # Create users and items from df
-    num_users, num_items = dataset.interactions_shape() # Get shape
-    interaction_list = list(df[df > 0].stack().index) # Get interaction pairs
-    interactions, weights = dataset.build_interactions((x[0], x[1]) for x in interaction_list) # Build interactions
+def get_attributes(tags=None, hike_difficulty=None, hike_distance=None, hike_elevation=None, hike_type=None):
+    '''Generates a list of hike attributes/tags the user has selected.
     
-    return dataset, interactions
-
-def lightfm_train(train, num_components, num_epochs):
-    NUM_THREADS = 1
-    NUM_COMPONENTS = num_components
-    NUM_EPOCHS = num_epochs
-    ITEM_ALPHA = 1e-6
-
-    # Let's fit a WARP model: these generally have the best performance.
-    model = LightFM(loss='warp',
-                    item_alpha=ITEM_ALPHA,
-                   no_components=NUM_COMPONENTS)
-
-    # Run 3 epochs and time it.
-    model = model.fit(train, epochs=NUM_EPOCHS, num_threads=NUM_THREADS)
+    Returns: list of individual hike attributes/tags.'''
     
-    return model
-
-def find_item(lfm_map, lfm_index):
-    return [ key for key in lfm_map.keys() if lfm_map[key] == lfm_index ][0]
-
-def return_top_10_new_user(interactions, model, item_map):
-    '''This is for recommending based on interactions'''
-    predictions = model.predict(interactions._shape[0]-1, np.arange(interactions._shape[1]))
-    sorted_by_index = (-1*predictions).argsort()
-    names = []
+    hike_attributes = []
     
-    for pred_num in range(10):
-        name = find_item(item_map, sorted_by_index[pred_num])
-        print('Prediction {} is {}.'.format(pred_num+1, name))
-        names.append(name)
+    if tags != ['']:
+        for tag in tags:
+            hike_attributes.append(tag)
+    
+    if hike_difficulty == 'Easy':
+        hike_attributes.append('difficulty_easy')
+    elif hike_difficulty == 'Moderate':
+        hike_attributes.append('difficulty_moderate')
+    elif hike_difficulty == 'Hard':
+        hike_attributes.append('difficulty_hard') 
         
-    return names
+    if hike_distance == 'Short':
+        hike_attributes.append('distance_short')
+    elif hike_difficulty == 'Medium':
+        hike_attributes.append('distance_medium')
+    elif hike_difficulty == 'Long':
+        hike_attributes.append('distance_long')
+        
+    if hike_elevation == 'Easy':
+        hike_attributes.append('elevation_easy')
+    elif hike_elevation == 'Medium':
+        hike_attributes.append('elevation_medium')
+    elif hike_elevation == 'Hard':
+        hike_attributes.append('elevation_hard')
+        
+    if hike_type == 'Out-and-Back':
+        hike_attributes.append('out_and_back')
+    elif hike_type == 'Point-to-Point':
+        hike_attributes.append('point_to_point')
+    elif hike_type == 'Loop':
+        hike_attributes.append('loop')
+        
+    return hike_attributes
+
+def get_item_feature_embeddings(hike_attributes, item_features_map, item_features_embeddings):
+    '''Generate embeddings for selected hike attributes (item features).
     
+    Returns: list of embeddings for each attribute (item feature).'''
+    
+    selected_item_features_embeddings = []
+    for hike_attribute in hike_attributes:
+        selected_item_features_embeddings.append(item_features_embeddings[item_features_map[hike_attribute]])
+    
+    return selected_item_features_embeddings
+
+def get_item_embeddings(selected_hikes, item_map, item_embeddings):
+    '''Generate embeddings for selected hikes (items)
+    
+    Returns: list of embeddings for each hike (item)'''
+    
+    selected_hike_embeddings = []
+    for selected_hike in selected_hikes:
+        selected_hike_embeddings.append(item_embeddings[item_map[selected_hike]])
+        
+    return selected_hike_embeddings
+
+def get_average_item_embedding(selected_item_embeddings):
+    '''Calculate the average embedding over all selected hikes (items).
+    
+    Returns: np.array of average embedding.'''
+
+    av_item_embedding = np.zeros((10,))
+    for item_embedding in selected_item_embeddings:
+        av_item_embedding += item_embedding
+    try:
+        av_item_embedding = av_item_embedding / len(selected_item_embeddings)
+        return av_item_embedding
+    except:
+        av_item_embedding = 0
+        return av_item_embedding
+
+def get_final_embedding(av_item_embedding, selected_item_features_embeddings):
+    '''Add all selected item feature embeddings to the average item embedding to produce the final embedding.
+    
+    Returns: np.array of final embedding'''
+    
+    final_embedding = av_item_embedding.copy()
+    for item_feature_embedding in selected_item_features_embeddings:
+        final_embedding += item_feature_embedding
+        
+    return final_embedding
+
+def find_item(item_map, item_index):
+    '''Get hike (item) name given a LightFM index.
+    
+    Returns: hike name (str)'''
+    
+    return [ key for key in item_map.keys() if item_map[key] == item_index ][0]
+
 def cos_sim(a, b):
+    '''Calculate the cosine similarity between two vectors.
+    
+    Returns: float'''
+    
     cos_sim = dot(a, b)/(norm(a)*norm(b))
     return cos_sim
 
-def calculate_similarities(model, hike_index_of_interest):
-    item_embeddings = model.get_item_representations()[1]
+def calculate_similarities(model, embedding, item_embeddings):
+    '''Calculate the similarity scores for a given embedding and all item embeddings.
+    
+    Returns: list of similarities, in order of LightFM index'''
+    
     sim_scores = []
     for item_index in range(item_embeddings.shape[0]):
-        sim_scores.append(cos_sim(item_embeddings[item_index, :], item_embeddings[hike_index_of_interest, :]))
+        sim_scores.append(cos_sim(item_embeddings[item_index], embedding))
         
     return sim_scores
 
-def get_top_hikes_by_similarity(sim_scores, item_map):
-    '''This is for recommending based on item similarities'''
+def get_top_hikes_by_similarity(sim_scores, item_map, num_hikes):
+    '''Find the top num_hikes hikes given a list of similarity scores.
+    
+    Returns: list of hike names.'''
+    
     top_items = (-1*np.array(sim_scores)).argsort()
     names = []
-    for pred_num in range(10):
+    for pred_num in range(num_hikes):
         name = find_item(item_map, top_items[pred_num])
         names.append(name)
     
     return names
-   
-   
+
+def get_hike_features(hike_name, hikes_features_df):
+    '''Get a list of item features for a hike
+        
+    Returns: list of hike_features'''
+    hike_features = []
+    for feature in hikes_features_df.columns:
+        if hikes_features_df.loc[hike_name, feature] == 1.0:
+            hike_features.append(feature)
+            
+    return hike_features
+
+def convert_features(hike_features):
+    converted_features = {}
+    converted_tags = []
+    for feature in hike_features:
+        if feature.startswith('difficulty'):
+            converted_features['Difficulty'] = feature.split('_')[1].capitalize()
+        elif feature.startswith('distance'):
+            converted_features['Distance'] = feature.split('_')[1].capitalize()
+        elif feature.startswith('elevation'):
+            converted_features['Elevation'] = feature.split('_')[1].capitalize()
+        elif feature == 'out_and_back':
+            converted_features['Type'] = 'Out-and-Back'
+        elif feature == 'point_to_point':
+            converted_features['Type'] = 'Point-to-Point'
+        elif feature == 'loop':
+            converted_features['Type'] = 'Loop'
+        else:
+            converted_tags.append(feature.capitalize())
+    if converted_tags:
+        converted_features['tags'] = converted_tags
+        
+    return converted_features
+
+def get_matched_features(converted_features, hike_difficulty, hike_distance, hike_elevation, hike_type, tags):
+    matched_features = {}
+    matched_tags = []
+    if converted_features['Difficulty'] == hike_difficulty:
+        matched_features['Difficulty'] = hike_difficulty
+    if converted_features['Distance'] == hike_distance:
+        matched_features['Distance'] = hike_distance
+    if converted_features['Elevation'] == hike_elevation:
+        matched_features['Elevation'] = hike_elevation
+    if converted_features['Type'] == hike_type:
+        matched_features['Type'] = hike_type
+    if converted_features['tags']:
+        for tag in converted_features['tags']:
+            if tag in tags:
+                matched_tags.append(tag)
+    if matched_tags:
+        matched_features['Tags'] = matched_tags
+    
+    return matched_features
